4fd35d5fc18947a967cea0c3bb40c21e7834d5b4
BrandonWernham <bwernham@uwo.ca>
2021-11-03 11:30:41 -0400

    Added set password button

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-
+  var pwInput = document.getElementsByClassName('pw-input');
   return (
     <div>
       <div className="title">Password Simulator</div>
       <div className="enter-pw-text">Enter a password:</div>
       <input className="pw-input"></input>
+      <button className="set-pw-btn">Set Password</button>
     </div>
   );
 }
